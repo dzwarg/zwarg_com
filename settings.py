a67da21b6e41be1d6ed6ea3c3ec5a1e14f1980e3
David Zwarg <david.zwarg@gmail.com>
2013-12-15 23:04:57 -0500

    Moved 'env' directives out of nginx config; use http for s3

--- settings.py
+++ settings.py
@@ -176,6 +176,8 @@ AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
 AWS_STORAGE_BUCKET_NAME = ''
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_S3_SECURE_URLS = False
+AWS_QUERYSTRING_AUTH = False
 
 #########
 # PATHS #
